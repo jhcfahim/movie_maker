7205147bc9dc53682e853770c901900814d756b6
Fahim Firdaus <108272834+jhcfahim@users.noreply.github.com>
2023-02-22 07:49:28 +0000

    added a function to make the question part cleaner

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-want_instructions = input("Do you want instructions? (yes/no) ").lower()
-if want_instructions == "yes" or want_instructions == "y":
+# Function
+def yes_no(question):
+
+  while True:
+    response = input(question).lower()
+
+    if response == "yes" or response == "y":
+      return "yes"
+    elif response == "no" or response == "n":
+      return "no"
+    else:
+      print("Please enter yes or no")
+
+# Main Routine
+want_instructions = yes_no("Do you want instructions? (yes/no) ").lower()
+
+if want_instructions == "yes":
   print("Instructions go here")
-elif want_instructions == "no" or want_instructions == "n":
-  pass
-else:
-  print("Please enter yes or no")
+
+print("We are done")
