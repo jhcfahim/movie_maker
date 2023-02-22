256f0f9e86f84fb751675bb7ff6d5e55980d511b
Fahim Firdaus <108272834+jhcfahim@users.noreply.github.com>
2023-02-22 07:56:59 +0000

    added a ticket system that makes sure that you cannot buy more tickets than the max. max tickets is 3 just for testing purposes, and will be changed later on

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def yes_no(question):
     else:
       print("Please enter yes or no")
 
+
 # Main Routine
 want_instructions = yes_no("Do you want instructions? (yes/no) ").lower()
 
@@ -18,3 +19,22 @@ if want_instructions == "yes":
   print("Instructions go here")
 
 print("We are done")
+
+# Ticket Looping
+MAX_TICKETS = 3
+
+tickets_sold = 0
+while tickets_sold < MAX_TICKETS:
+  name = input("Please enter your name or 'xxx' to quit: ")
+
+  if name == "xxx":
+    break
+
+  tickets_sold += 1
+
+if tickets_sold == MAX_TICKETS:
+  print("Congratulations, you have sold all the tickets.")
+else:
+  print(
+    f"You have sold {tickets_sold} ticket's. There is {MAX_TICKETS - tickets_sold} ticket's remaining."
+  )
